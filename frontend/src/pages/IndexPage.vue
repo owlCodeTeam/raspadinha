e549231d4c335b6558dc63383a85fed8767a7035
henriqueWebDev <henriquedevprofessional@gmail.com>
2024-01-19 16:45:22 -0400

    start develop QrReader

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,16 +1,21 @@
 <template>
   <q-page class="row items-center justify-evenly">
+    <!--
     <ScratchCard />
+    -->
+    <QrReader />
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import ScratchCard from '../components/ScratchCard.vue'
+//import ScratchCard from '../components/ScratchCard.vue'
+import QrReader from 'src/components/QrReader.vue';
 export default defineComponent({
   name: 'IndexPage',
   components: {
-    ScratchCard
+    //ScratchCard,
+    QrReader
   }
 });
 </script>
